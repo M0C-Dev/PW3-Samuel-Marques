dcbbb6438125ca471d08fde830515e6ed0976389
Marques 👽 <71507756+M0C-Dev@users.noreply.github.com>
2026-03-18 13:41:51 -0300

    tabela ativ 2

---
+++ SPAs/Atividade 2/script.js
@@ -0,0 +1,14 @@
+const listaDeProdutos = [
+    {nome: "Camiseta Regata", preco: 19.90},
+    {nome: "Camiseta Social", preco: 59.90},
+    {nome: "Blusa", preco: 199.90},
+    {nome: "Pullover", preco: 39.90},
+    {nome: "Sapato", preco: 99.90},
+    {nome: "Calça", preco: 38.90},
+    {nome: "Meias", preco: 9.90},
+    {nome: "Luvas", preco: 24.90},
+    {nome: "Jaqueta", preco: 329.90},
+    {nome: "Bermuda", preco: 69.90},
+    {nome: "Chinelo", preco: 14.90},
+    {nome: "Boné", preco: 6.90}
+];
