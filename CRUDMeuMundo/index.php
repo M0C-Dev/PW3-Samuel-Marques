2b7266b9036b91fba02c27dd3426f4c98bd7f689
ghnsoares <71507756+M0C-Dev@users.noreply.github.com>
2026-06-01 14:20:53 -0300

    meu mundo

---
+++ CRUDMeuMundo/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Mundo Express!</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="container">
+        <header>
+            <img src="imgs/MeuMundoLogo.png" alt="">
+            <span id="logo">Meu Mundo Express!</span>
+        </header>
+        <nav>
+            <span class="miniTitle">Bem-vindo ao <b>Meu Mundo Express!</b></span>
+        </nav>
+        <main>
+        </main>
+        <footer>
+            <hr>
+            <p>Por Samuel M. 2026 ©</p>
+            <hr>
+        </footer>
+    </div>
+</body>
+</html>
